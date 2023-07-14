fc4d47d5a8f92bdf8b1240c9f0dafa747c8be7d9
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2023-07-15 00:00:20 +0530

    change in category

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import "./Category.scss"
 import {categoryImage} from "../../utils/image"
 
@@ -16,7 +15,7 @@ const Category = () => {
             <div className = "category-items grid">
                 {
                     categoryImage.map(category => (
-                        <Link to = {`category/${category.category}`} key = {category.id}>
+                        
                             <div className = "category-item" >
                                 <div className='category-item-img'>
                                     <img src = {category.image} alt = "" />
@@ -25,7 +24,6 @@ const Category = () => {
                                     <h6 className='fs-20'>{category.title}</h6>
                                 </div>
                             </div>
-                        </Link>
                     ))
                 }
                 
